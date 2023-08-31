64fea0a1555046659d6fb3d15469be8f350ad766
Aidin <bekturganovaidin@gmail.com>
2023-08-31 20:38:44 +0600

    Initial Commit

---
+++ HackathonSchoolProj/HackathonSchoolProjApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackathonSchoolProjApp.swift
+//  HackathonSchoolProj
+//
+//  Created by Aidin Bekturganov on 31/8/23.
+//
+
+import SwiftUI
+
+@main
+struct HackathonSchoolProjApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
